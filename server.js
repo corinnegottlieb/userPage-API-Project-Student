c71121c891072c75c1ae9bf76bd50a5efe2e25db
corinnegottleb <corinneottlieb@gmail.com>
2019-01-27 13:40:37 +0200

    first version complete

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+const path = require('path')
+
+
+app.use(express.static(path.join(__dirname, 'dist')))
+
+const port = 3000
+app.listen(port, function(){
+    console.log(`Running server on port ${port}`)
+})
